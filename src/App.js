5ffa427345fc7aba539d4151ac53f1cfe8d912b5
TomasZorzo123 <zorzolot@gmail.com>
2021-08-18 16:36:35 -0300

    DES-AD-02: Tabla de Productos, primera version finalizada

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import ProductTable from "./components/ProducTable";
 const App = ()=> {
   return (
     <div>
       <h1>FoodFast - Web App</h1>
+      <ProductTable/>
     </div>
   );
 }
---
+++ src/components/ProducTable.js
@@ -0,0 +1,94 @@
+import MaterialTable from "material-table";
+
+
+let mock=require("./mock.json")
+let columns1 =[
+  { title: 'Nombre', field: 'optionName' },
+  { title: 'Precio Unitario', field: 'unitPrice', type: 'currency' },
+  { title: '¿Disponible?', field: 'isAvailable', type: 'boolean' },
+]
+const ProductTable = ()=> {
+
+        return (
+          <div>
+          <MaterialTable
+            title="Productos"
+            columns={columns1}
+            data={mock.menuOptions}
+            actions={[
+              {
+                icon: 'edit',
+                tooltip: 'Edit Item',
+                //onClick: (event, rowData) => alert("You saved " + rowData.name)
+              },
+              rowData => ({
+                icon: 'delete',
+                tooltip: 'Delete Item',
+                //onClick: (event, rowData) => confirm("You want to delete " + rowData.name),
+                //disabled: rowData.birthYear < 2000
+              })
+            ]}
+            options={{
+              actionsColumnIndex: -1
+            }}
+            localization={{
+              body: {
+                  emptyDataSourceMessage: "No hay datos para mostrar",
+                  addTooltip: 'Agregar',
+                  deleteTooltip: 'Eliminar',
+                  editTooltip: 'Editar',
+                  filterRow: {
+                      filterTooltip: 'Filtrar'
+                  },
+                  editRow: {
+                      deleteText: 'Seguro que quiere borrar esta linea?',
+                      cancelTooltip: 'Cancelar',
+                      saveTooltip: 'Guardar'
+                  }
+              },
+              grouping: {
+                  placeholder: "Titulo del encabezado ...",
+                  groupedBy: 'Agrupar por:'
+              },
+              header: {
+                  actions: 'Acciones'
+              },
+              pagination: {
+                  labelDisplayedRows: '{from}-{to} de {count}',
+                  labelRowsSelect: 'lineas',
+                  labelRowsPerPage: 'lineas por páginas:',
+                  firstAriaLabel: 'Primer página',
+                  firstTooltip: 'Primer página',
+                  previousAriaLabel: 'Página anterior',
+                  previousTooltip: 'Página anterior',
+                  nextAriaLabel: 'Página siguiente',
+                  nextTooltip: 'Página siguiente',
+                  lastAriaLabel: 'Última página',
+                  lastTooltip: 'Última página'
+              },
+              toolbar: {
+                  addRemoveColumns: 'Agregar o quitar columnas',
+                  nRowsSelected: '{0} linea(s) seleccionada(s)',
+                  showColumnsTitle: 'Ver columnas',
+                  showColumnsAriaLabel: 'Ver columnas',
+                  exportTitle: 'Exportar',
+                  exportAriaLabel: 'Exportar',
+                  exportName: 'Exportar a CSV',
+                  searchTooltip: 'Buscar',
+                  searchPlaceholder: 'Buscar'
+              }
+          }}
+
+
+
+
+
+          />
+          </div>
+        )
+      }
+
+
+
+  
+  export default ProductTable;
