783acfa6706ffc72984a46d5754685a7ea7fe6d6
gabrielgolzman <gabrielgolzman@gmail.com>
2021-08-20 15:14:59 -0300

    fixed some code inconsistencies and moved the mock.json to a new data folder inside the stc

--- src/components/ProducTable.js
+++ src/components/ProducTable.js
@@ -1,59 +1,57 @@
-import MaterialTable from "material-table";
+import MaterialTable from 'material-table';
 
-
-let mock=require("./mock.json")
-let columns1 =[
-  { title: 'Nombre', field: 'optionName' },
-  { title: 'Precio Unitario', field: 'unitPrice', type: 'currency' },
-  { title: '¿Disponible?', field: 'isAvailable', type: 'boolean' },
-]
-const ProductTable = ()=> {
-
-        return (
-          <div>
-          <MaterialTable
+let mock = require('../data/mock.json');
+let columns1 = [
+   { title: 'Nombre', field: 'optionName' },
+   { title: 'Precio Unitario', field: 'unitPrice', type: 'currency' },
+   { title: '¿Disponible?', field: 'isAvailable', type: 'boolean' },
+];
+const ProductTable = () => {
+   return (
+      <div>
+         <MaterialTable
             title="Productos"
             columns={columns1}
             data={mock.menuOptions}
             actions={[
-              {
-                icon: 'edit',
-                tooltip: 'Edit Item',
-                //onClick: (event, rowData) => alert("You saved " + rowData.name)
-              },
-              rowData => ({
-                icon: 'delete',
-                tooltip: 'Delete Item',
-                //onClick: (event, rowData) => confirm("You want to delete " + rowData.name),
-                //disabled: rowData.birthYear < 2000
-              })
+               {
+                  icon: 'edit',
+                  tooltip: 'Edit Item',
+                  //onClick: (event, rowData) => alert("You saved " + rowData.name)
+               },
+               (rowData) => ({
+                  icon: 'delete',
+                  tooltip: 'Delete Item',
+                  //onClick: (event, rowData) => confirm("You want to delete " + rowData.name),
+                  //disabled: rowData.birthYear < 2000
+               }),
             ]}
             options={{
-              actionsColumnIndex: -1
+               actionsColumnIndex: -1,
             }}
             localization={{
-              body: {
-                  emptyDataSourceMessage: "No hay datos para mostrar",
+               body: {
+                  emptyDataSourceMessage: 'No hay datos para mostrar',
                   addTooltip: 'Agregar',
                   deleteTooltip: 'Eliminar',
                   editTooltip: 'Editar',
                   filterRow: {
-                      filterTooltip: 'Filtrar'
+                     filterTooltip: 'Filtrar',
                   },
                   editRow: {
-                      deleteText: 'Seguro que quiere borrar esta linea?',
-                      cancelTooltip: 'Cancelar',
-                      saveTooltip: 'Guardar'
-                  }
-              },
-              grouping: {
-                  placeholder: "Titulo del encabezado ...",
-                  groupedBy: 'Agrupar por:'
-              },
-              header: {
-                  actions: 'Acciones'
-              },
-              pagination: {
+                     deleteText: 'Seguro que quiere borrar esta linea?',
+                     cancelTooltip: 'Cancelar',
+                     saveTooltip: 'Guardar',
+                  },
+               },
+               grouping: {
+                  placeholder: 'Titulo del encabezado ...',
+                  groupedBy: 'Agrupar por:',
+               },
+               header: {
+                  actions: 'Acciones',
+               },
+               pagination: {
                   labelDisplayedRows: '{from}-{to} de {count}',
                   labelRowsSelect: 'lineas',
                   labelRowsPerPage: 'lineas por páginas:',
@@ -64,9 +62,9 @@ const ProductTable = ()=> {
                   nextAriaLabel: 'Página siguiente',
                   nextTooltip: 'Página siguiente',
                   lastAriaLabel: 'Última página',
-                  lastTooltip: 'Última página'
-              },
-              toolbar: {
+                  lastTooltip: 'Última página',
+               },
+               toolbar: {
                   addRemoveColumns: 'Agregar o quitar columnas',
                   nRowsSelected: '{0} linea(s) seleccionada(s)',
                   showColumnsTitle: 'Ver columnas',
@@ -75,20 +73,12 @@ const ProductTable = ()=> {
                   exportAriaLabel: 'Exportar',
                   exportName: 'Exportar a CSV',
                   searchTooltip: 'Buscar',
-                  searchPlaceholder: 'Buscar'
-              }
-          }}
+                  searchPlaceholder: 'Buscar',
+               },
+            }}
+         />
+      </div>
+   );
+};
 
-
-
-
-
-          />
-          </div>
-        )
-      }
-
-
-
-  
-  export default ProductTable;
+export default ProductTable;
