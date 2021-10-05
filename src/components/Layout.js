3ddf629d41945ba0e885b6f9951b8fd3f680eec5
gabrielgolzman <gabrielgolzman@gmail.com>
2021-10-05 17:25:11 -0300

    DES-AD-01 : changed restaurant name

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,48 +11,61 @@ import Typography from '@mui/material/Typography';
 
 const drawerWidth = 240;
 const drawer = (
-    <div>
+   <div>
       <Toolbar />
-      <Typography sx={{marginLeft:'20px'}} variant="div" noWrap component="div">
-       Rick's Burgers
+      <Typography
+         sx={{ marginLeft: '20px' }}
+         variant="div"
+         noWrap
+         component="div"
+      >
+         Restaurant
       </Typography>
       <Divider />
-      <List sx={{ marginLeft: "20px"}}>
-        {['Carta', 'Pedidos', 'Reservas', 'Mozos'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+      <List sx={{ marginLeft: '20px' }}>
+         {['Carta', 'Pedidos', 'Reservas', 'Mozos'].map((text, index) => (
+            <ListItem button key={text}>
+               <ListItemText primary={text} />
+            </ListItem>
+         ))}
       </List>
-    </div>
-  );
+   </div>
+);
 const Layout = (props) => {
-
-    return (
-        <Box sx={{ display: 'flex'}}>
-          <CssBaseline />
-          <Box
+   return (
+      <Box sx={{ display: 'flex' }}>
+         <CssBaseline />
+         <Box
             component="nav"
-            sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 }, marginLeft: "10px" }}
+            sx={{
+               width: { sm: drawerWidth },
+               flexShrink: { sm: 0 },
+               marginLeft: '10px',
+            }}
             aria-label="mailbox folders"
-          >
+         >
             <Drawer
-              variant="permanent"
-              sx={{
-                display: { xs: 'none', sm: 'block' },
-                '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth, background:"linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(222,95,16,1) 32%)" , color:'white'},
-              }}
-              open
+               variant="permanent"
+               sx={{
+                  display: { xs: 'none', sm: 'block' },
+                  '& .MuiDrawer-paper': {
+                     boxSizing: 'border-box',
+                     width: drawerWidth,
+                     background:
+                        'linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(222,95,16,1) 32%)',
+                     color: 'white',
+                  },
+               }}
+               open
             >
-              {drawer}
+               {drawer}
             </Drawer>
-          </Box>
-          <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-                {props.children}
-          </Box>
-        </Box>
-      );
-}
+         </Box>
+         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+            {props.children}
+         </Box>
+      </Box>
+   );
+};
 
-
-export default Layout
+export default Layout;
