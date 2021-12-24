64949565b2893fbc2df8b9a74240154e81f28fbf
gabrielgolzman <gabrielgolzman@gmail.com>
2021-12-23 23:48:05 -0300

    Phant commit 2 test

--- src/components/util/ui/Layout/Layout.js
+++ src/components/util/ui/Layout/Layout.js
@@ -14,6 +14,7 @@ import { AuthenticationContext } from '../../../../services/auth/authentication.
 
 const Layout = ({ children }) => {
    const { onLogout } = useContext(AuthenticationContext);
+   console.log('hello')
 
    const Logout = () => {
       onLogout();
